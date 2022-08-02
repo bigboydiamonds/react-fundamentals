58ebc78ea7c81e47974948ff4ce63fd8210580b4
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-08-02 15:50:02 -0700

    complete exercise 06 ec 2

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,23 +4,19 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-  // 🐨 add a submit event handler here (`handleSubmit`).
-
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
-
-  // 🐨 add the onSubmit handler to the <form> below
-
-  // 🐨 make sure to associate the label to the input.
-  // to do so, set the value of 'htmlFor' prop of the label to the id of input
+  // create state to keep track of input validation error
+  const [error, setError] = React.useState(null)
   const inputRef = React.useRef(null)
+
+  //handleChange function to validated user input
+  //validate if input is lowercase
+  //set error state if input is invalid
+  const handleChange = event => {
+    event.preventDefault()
+    const value = event.target.value
+    const isValid = value === value.toLowerCase()
+    setError(isValid ? null : 'Username must be lower case')
+  }
 
   const handleSubmit = event => {
     event.preventDefault()
@@ -32,9 +28,19 @@ function UsernameForm({onSubmitUsername}) {
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="usernameInput">Username:</label>
-        <input id="usernameInput" type="text" ref={inputRef} />
+        <input
+          id="usernameInput"
+          type="text"
+          ref={inputRef}
+          onChange={handleChange}
+        />
+        {error && (
+          <div style={{color: 'red'}} role="alert">
+            input must be lowercase{' '}
+          </div>
+        )}
       </div>
-      <button type="submit" disabled={true}>
+      <button type="submit" disabled={error}>
         Submit
       </button>
     </form>
